53d8e9eab28ef4934480b0e9a9c11a7eb46f32c3
nance-tech <nycmbugua@gmail.com>
2024-05-06 09:06:34 +1000

    Added additional functionality to calculator

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const logger = require("./logger");
 
 app.get("", (_, res) => {
-  logger.info("health checking endpoint")
+  logger.info("health checking endpoint");
   res.status(200).json({ message: "Calculator Microservice Running" });
 });
 
@@ -14,7 +14,7 @@ app.get("/add/:num1/:num2", (req, res) => {
   num1 = Number(num1);
   num2 = Number(num2);
   let errorMsg = undefined;
-  
+
   if (isNaN(num1)) {
     errorMsg = "Num1 Must be a Number";
   }
@@ -39,7 +39,7 @@ app.get("/subtract/:num1/:num2", (req, res) => {
   num1 = Number(num1);
   num2 = Number(num2);
   let errorMsg = undefined;
-  
+
   if (isNaN(num1)) {
     errorMsg = "Num1 Must be a Number";
   }
@@ -64,7 +64,7 @@ app.get("/multiply/:num1/:num2", (req, res) => {
   num1 = Number(num1);
   num2 = Number(num2);
   let errorMsg = undefined;
- 
+
   if (isNaN(num1)) {
     errorMsg = "Num1 Must be a Number";
   }
@@ -102,6 +102,73 @@ app.get("/divide/:num1/:num2", (req, res) => {
     return res.status(400).json({ error: errorMsg });
   }
   const result = Number(num1) / Number(num2);
+  return res.status(200).json({ result });
+});
+
+// Square Root
+app.get("/sqrt/:num1", (req, res) => {
+  let { num1 } = req.params;
+  logger.info(`New square root operation has been requested: ${num1}`);
+  num1 = Number(num1);
+  let errorMsg = undefined;
+
+  if (isNaN(num1)) {
+    errorMsg = "Num1 Must be a Number";
+  }
+  if (errorMsg) {
+    logger.error(`An error has been encountered: ${errorMsg}`);
+    return res.status(400).json({ error: errorMsg });
+  }
+  const result = Math.sqrt(num1);
+  return res.status(200).json({ result });
+});
+
+// Modulo
+app.get("/modulo/:num1/:num2", (req, res) => {
+  let { num1, num2 } = req.params;
+  logger.info(`New modulo operation has been requested: ${num1} % ${num2}`);
+  num1 = Number(req.params.num1);
+  num2 = Number(req.params.num2);
+  let errorMsg = undefined;
+
+  if (isNaN(num1)) {
+    errorMsg = "Num1 Must be a Number";
+  }
+  if (isNaN(num2)) {
+    errorMsg = "Num2 Must be a Number";
+  }
+  if (num2 === 0) {
+    errorMsg = "You cannot divide a number by Zero";
+  }
+
+  if (errorMsg) {
+    logger.error(`An error has been encountered: ${errorMsg}`);
+    return res.status(400).json({ error: errorMsg });
+  }
+  const result = num1 % num2;
+  return res.status(200).json({ result });
+});
+
+// Exponential
+app.get("/exponential/:num1/:num2", (req, res) => {
+  let { num1, num2 } = req.params;
+  logger.info(`New Exponential operation has been requested: ${num1}**${num2}`);
+  num1 = Number(req.params.num1);
+  num2 = Number(req.params.num2);
+  let errorMsg = undefined;
+
+  if (isNaN(num1)) {
+    errorMsg = "Num1 Must be a Number";
+  }
+  if (isNaN(num2)) {
+    errorMsg = "Num2 Must be a Number";
+  }
+
+  if (errorMsg) {
+    logger.error(`An error has been encountered: ${errorMsg}`);
+    return res.status(400).json({ error: errorMsg });
+  }
+  const result = num1 ** num2;
   return res.status(200).json({ result });
 });
 
